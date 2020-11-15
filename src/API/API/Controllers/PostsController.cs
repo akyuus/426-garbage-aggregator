4f014bb7272c0846b18fe11462278a9126ae6bd4
komaano <asheamar@gmail.com>
2020-11-14 23:03:47 -0500

    Chunk of the backend completed

---
+++ src/API/API/Controllers/PostsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        // GET: api/Posts?type
+        [Route("Posts")]
+        [HttpGet]
+        public string Get(string type = "all", string site = "both", string sort = "score", bool asc = false)
+        {
+            return $"{type}, {site}, {sort}, {asc}";
+        }
+    }
+}
---
+++ src/API/API/Models/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Post
+    {
+        public string sourceURL { get; set; }
+        public string text { get; set; }
+        public int score { get; set; }
+        public DateTime date { get; set; }
+        public string author { get; set; }
+        public string source { get; set; } //reddit or twitter
+        public string type { get; set; } //text or image (if a post contains an image it will default to image)
+        public string[] mediaURLs { get; set; }
+        public Post(string sourceURL, int score, string author, string source, DateTime date, string type, string text = "", string[] mediaURLs = null)
+        {
+            this.sourceURL = sourceURL;
+            this.text = text;
+            this.score = score;
+            this.date = date;
+            this.author = author;
+            this.source = source;
+            this.type = type;
+            this.mediaURLs = mediaURLs;
+        }
+    }
+}
